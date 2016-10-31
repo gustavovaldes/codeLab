10ebd6cbae968f43fbfa0d52311dc117e633b63d
gustavovaldes <g.valdes.aracena@gmail.com>
2016-10-31 02:24:40 -0300

    add TODO to include variation of problem

--- src/main/java/com/guga/interviewbit/arrays/MaxSumContiguousSubarray.java
+++ src/main/java/com/guga/interviewbit/arrays/MaxSumContiguousSubarray.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 /**
  * Created by guga
+ *
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ *
+ * For example:
+ *
+ * Given the array [-2,1,-3,4,-1,2,1,-5,4],
+ *
+ * the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ *
+ * For this problem, return the maximum sum.
  */
 public class MaxSumContiguousSubarray {
 
-    public int maxSubArray(final List<Integer> a) {
+    public int maxSubArray(final List<Integer> a) { //TODO include variation to return the range too.
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < a.size(); i++) {
