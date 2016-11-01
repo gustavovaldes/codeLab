08ba420669df442b74c0d025c52b8f6799ec668f
gustavovaldes <g.valdes.aracena@gmail.com>
2016-11-01 20:41:38 -0300

    Remove duplicates in LinkedList, add option without hashmap but O(n^2)

--- src/main/java/com/guga/ctci/linkedlists/P21_RemoveDups.java
+++ src/main/java/com/guga/ctci/linkedlists/P21_RemoveDups.java
@@ -7,6 +7,7 @@ import java.util.Map;
  * Created by guga
  *
  * Write code to remove duplicates from an unsorted linked list
+ * How would you solve this problem if a temporary buffer is not allowed.
  */
 public class P21_RemoveDups {
 
@@ -17,6 +18,12 @@ public class P21_RemoveDups {
         printLinkedList(head);
         removeDups(head);
         printLinkedList(head);
+
+        Node head2 = new Node<Integer>(4);
+        head2.append(5).append(3).append(5).append(2); // 4->5->3->5->2
+        printLinkedList(head2);
+        removeDups(head2);
+        printLinkedList(head2);
 
 
     }
@@ -30,6 +37,9 @@ public class P21_RemoveDups {
     }
 
 
+    /**
+     * solution using HashMap O(n)
+     */
     public static Node removeDups(Node<Integer> node){
         Node head = node;
         Map<Integer,Integer> values = new HashMap<>();
@@ -45,6 +55,28 @@ public class P21_RemoveDups {
         }
         return head;
     }
+
+    /**
+     * Solution without HashMap, but O(n^2)
+     */
+    public static Node removeDupsNoBuffer(Node<Integer> node){//TODO add test, can i remove parent reference? clean code?
+        Node head = node;
+        Node visitor = null;
+        Node parent = null;
+        while(node!=null){
+            visitor= node.next;
+            parent = node;
+            while (visitor!=null){
+                if(visitor.item==node.item){
+                    removeChild(parent,visitor);
+                }
+                parent= visitor;
+                visitor= visitor.next;
+            }
+        }
+        return head;
+    }
+
 
     public static void removeChild(Node parent, Node child){
         parent.next = child.next;
